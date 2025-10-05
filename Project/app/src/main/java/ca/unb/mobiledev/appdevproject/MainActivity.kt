2e255611ce41408cd48d09dbd0e574f3999ea24f
eptilica <nicholas3320@gmail.com>
2025-10-05 15:39:32 -0300

    add qr scan button
    added button to scan a qr code in main activity. data scanned appears as a toast

--- Project/app/src/main/java/ca/unb/mobiledev/appdevproject/MainActivity.kt
+++ Project/app/src/main/java/ca/unb/mobiledev/appdevproject/MainActivity.kt
@@ -1,33 +1,73 @@
 package ca.unb.mobiledev.appdevproject
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import ca.unb.mobiledev.appdevproject.ui.theme.AppDevProjectTheme
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AppDevProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val scanButton : Button = findViewById(R.id.scanButton)
+
+        scanButton.setOnClickListener {
+            scanQRCode(this)
         }
     }
+}
+
+fun scanQRCode(context : Context) {
+    //configure options for qrcode scanner
+    val options = GmsBarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_QR_CODE)
+        .build()
+
+    //instantiate scanner
+    val scanner = GmsBarcodeScanning.getClient(context, options)
+
+    //start scan and handle results
+    scanner.startScan()
+        .addOnSuccessListener { barcode ->
+            val rawValue: String? = barcode.rawValue
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(context, rawValue, duration) // in Activity
+            toast.show()
+        }
+        .addOnCanceledListener {
+            val text = "Canceled"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(context, text, duration) // in Activity
+            toast.show()
+        }
+        .addOnFailureListener { e ->
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(context, e.toString(), duration) // in Activity
+            toast.show()
+        }
 }
 
 @Composable
